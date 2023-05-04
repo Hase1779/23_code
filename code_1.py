d11234da2581c98b1023cfb0115fd225babb9aec
Hase1779 <128006281+Hase1779@users.noreply.github.com>
2023-05-04 10:29:43 +0900

    Add files via upload

---
+++ code_1.py
@@ -0,0 +1,9 @@
+totoal = 0 ;
+counter = 1;
+while counter <= 10:
+    graod = int(input("정수입력하시오: "));
+    yoyal = grade + total;
+    counter = counter + 1;
+    average = total / 10;
+    print("평균은",average,"입니다")10
+    
---
+++ code_2.py
@@ -0,0 +1,8 @@
+print("3개의 값을 입력하시오");
+x = int(input("x의갑을 입략:  "));
+y = int(input("y의갑을 입략:  "));
+z = int(input("z의갑을 입략:  "));
+x = x + 1;
+y = y + 1;
+z = z + 1;
+print("변경된 값 :",x,y,z)
